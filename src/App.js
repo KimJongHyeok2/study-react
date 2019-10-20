5c4aaa8c02e0b75465bfc39a255b0819385f2eaf
Kim Jonghyeok <proje_ct@naver.com>
2019-10-21 01:36:39 +0900

    Test Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
+// import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Page
+import { Home, About } from "./pages/pages";
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Router>
+          <Route exact path="/" component={Home}/>
+          <Switch>
+            <Route path="/about/:name" component={About}/>
+            <Route path="/about/" component={About}/>
+          </Switch>
+        </Router>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
